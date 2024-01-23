acc2cb76970f18cc83505181881d55e03dd6cbd7
PriteeAr0te <arotepritee111@gmail.com>
2024-01-23 10:09:51 +0530

    React component with Syncfusion Document Editor for DOCX format

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { DocumentEditorContainerComponent, Toolbar, Inject } from '@syncfusion/ej2-react-documenteditor';
 
 function App() {
+  let editorObject = DocumentEditorContainerComponent | null
+  const onSave = () =>{
+    editorObject?.documentEditor.save("Sample", "Docx")
+    editorObject?.documentEditor.save("Sample", "PDF")
+  }
   return (
     <div className="app">
+      <button onClick={onSave} style= {{marginBottom:10, fontSize:20, marginLeft:5, marginTop:5}}>Save</button>
+      <DocumentEditorContainerComponent ref = {(ins=>editorObject=ins)} height='590' enableToolbar={true}
+        serviceUrl ="https://ej2services.syncfusion.com/production/web-services/api/documenteditor/">
+        <Inject services={[Toolbar]}></Inject>
+      </DocumentEditorContainerComponent>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { registerLicense } from '@syncfusion/ej2-base';
+// import '@syncfusion/ej2-react-documenteditor/styles/material.css';
+
+registerLicense("Ngo9BigBOggjHTQxAR8/V1NAaF5cWWJCf0x0TXxbf1x0ZFJMYFpbRHRPMyBoS35RckViW35ed3RTRmJVU0Bx")
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
